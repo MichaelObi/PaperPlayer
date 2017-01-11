c431dbdc1f010494658d83a0401e16c313830030
Michael Obi <ask4myk@gmail.com>
2017-01-11 09:09:01 +0100

    Add MVP contract interface for AlbumActivity

---
+++ app/src/main/java/net/devdome/paperplayer/presentation/album/AlbumActivity.java
@@ -0,0 +1,12 @@
+package net.devdome.paperplayer.presentation.album;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * PaperPlayer
+ * Michael Obi
+ * 11 01 2017 7:47 AM
+ */
+
+public class AlbumActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/net/devdome/paperplayer/presentation/album/AlbumContract.java
@@ -0,0 +1,10 @@
+package net.devdome.paperplayer.presentation.album;
+
+/**
+ * PaperPlayer
+ * Michael Obi
+ * 11 01 2017 7:43 AM
+ */
+
+public class AlbumContract {
+}
