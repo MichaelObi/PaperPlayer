1656c7525a8dffd17227dc43c6851dbad8408f38
Michael Obi <ask4myk@gmail.com>
2017-05-08 21:37:45 +0100

    Migrate SquareImageView to Kotlin

---
+++ app/src/main/java/xyz/michaelobi/paperplayer/widget/SquareImageView.kt
@@ -0,0 +1,29 @@
+package xyz.michaelobi.paperplayer.widget
+
+import android.content.Context
+import android.util.AttributeSet
+
+/**
+ * PaperPlayer
+ * Michael Obi
+ * 08 05 2017 6:51 PM
+ */
+
+class SquareImageView : android.support.v7.widget.AppCompatImageView {
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val measuredWidth = measuredWidth
+        val measuredHeight = measuredHeight
+        if (measuredWidth > measuredHeight) {
+            setMeasuredDimension(measuredHeight, measuredHeight)
+        } else {
+            setMeasuredDimension(measuredWidth, measuredWidth)
+        }
+    }
+}
