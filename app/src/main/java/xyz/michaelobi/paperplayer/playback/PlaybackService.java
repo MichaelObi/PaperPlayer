a125bcbe6164d51066dfbe67bd3d6cf230faece7
Michael Obi <ask4myk@gmail.com>
2017-05-29 10:10:45 +0100

    Remove unused layout files

--- app/src/main/java/xyz/michaelobi/paperplayer/playback/PlaybackService.java
+++ app/src/main/java/xyz/michaelobi/paperplayer/playback/PlaybackService.java
@@ -22,230 +22,218 @@
  * SOFTWARE.
  */
 
-package xyz.michaelobi.paperplayer.playback;
+package xyz.michaelobi.paperplayer.playback
 
-import android.app.Service;
-import android.content.Context;
-import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.IBinder;
-import android.os.PowerManager;
-import android.util.Log;
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.IBinder
+import android.os.PowerManager
+import android.util.Log
 
-import java.io.IOException;
+import java.io.IOException
 
-import xyz.michaelobi.paperplayer.data.MusicRepositoryInterface;
-import xyz.michaelobi.paperplayer.event.EventBus;
-import xyz.michaelobi.paperplayer.injection.Injector;
-import xyz.michaelobi.paperplayer.playback.events.PlaybackState;
-import xyz.michaelobi.paperplayer.playback.events.action.NextSong;
-import xyz.michaelobi.paperplayer.playback.events.action.PlayAllSongs;
-import xyz.michaelobi.paperplayer.playback.events.action.PreviousSong;
-import xyz.michaelobi.paperplayer.playback.events.action.RequestPlaybackState;
-import xyz.michaelobi.paperplayer.playback.events.action.Seek;
-import xyz.michaelobi.paperplayer.playback.events.action.TogglePlayback;
-import xyz.michaelobi.paperplayer.playback.queue.QueueManager;
+import xyz.michaelobi.paperplayer.data.MusicRepositoryInterface
+import xyz.michaelobi.paperplayer.event.EventBus
+import xyz.michaelobi.paperplayer.injection.Injector
+import xyz.michaelobi.paperplayer.playback.events.PlaybackState
+import xyz.michaelobi.paperplayer.playback.events.action.NextSong
+import xyz.michaelobi.paperplayer.playback.events.action.PlayAllSongs
+import xyz.michaelobi.paperplayer.playback.events.action.PreviousSong
+import xyz.michaelobi.paperplayer.playback.events.action.RequestPlaybackState
+import xyz.michaelobi.paperplayer.playback.events.action.Seek
+import xyz.michaelobi.paperplayer.playback.events.action.TogglePlayback
+import xyz.michaelobi.paperplayer.playback.queue.QueueManager
 
 /**
  * PaperPlayer Michael Obi 11 01 2017 10:46 PM
  */
-public class PlaybackService extends Service implements MediaPlayer.OnErrorListener, AudioManager
-        .OnAudioFocusChangeListener, MediaPlayer.OnCompletionListener, MediaPlayer
-        .OnPreparedListener {
-    private static final String TAG = "PlaybackService";
-    QueueManager queueManager;
-    MusicRepositoryInterface musicRepository;
-    private EventBus eventBus;
-    private MediaPlayer player;
-    private int songSeek = 0;
+class PlaybackService : Service(), MediaPlayer.OnErrorListener, AudioManager.OnAudioFocusChangeListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener {
+    internal var queueManager: QueueManager
+    internal var musicRepository: MusicRepositoryInterface
+    private val eventBus: EventBus
+    private var player: MediaPlayer? = null
+    private var songSeek = 0
 
-    // TODO: Remove all PlaybackStarted and PlaybackStopped  events in favor of PlaybackState
-
-    public PlaybackService() {
-        eventBus = Injector.provideEventBus();
-        queueManager = Injector.provideQueueManager();
-        musicRepository = Injector.provideMusicRepository(this);
+    init {
+        eventBus = Injector.provideEventBus()
+        queueManager = Injector.provideQueueManager()
+        musicRepository = Injector.provideMusicRepository(this)
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        player = new MediaPlayer();
-        player.setOnCompletionListener(this);
-        player.setOnPreparedListener(this);
-        player.setOnErrorListener(this);
-        player.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
-        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        registerEvents();
-        musicRepository.getAllSongs().subscribe(songs -> queueManager.setQueue(songs, 0));
+    override fun onCreate() {
+        super.onCreate()
+        player = MediaPlayer()
+        player!!.setOnCompletionListener(this)
+        player!!.setOnPreparedListener(this)
+        player!!.setOnErrorListener(this)
+        player!!.setWakeMode(applicationContext, PowerManager.PARTIAL_WAKE_LOCK)
+        player!!.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        registerEvents()
+        musicRepository.allSongs.subscribe { songs -> queueManager.setQueue(songs, 0) }
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        eventBus.post(new RequestPlaybackState());
-        return START_STICKY;
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        eventBus.post(RequestPlaybackState())
+        return Service.START_STICKY
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    override fun onBind(intent: Intent): IBinder? {
+        return null
     }
 
-    @Override
-    public void onAudioFocusChange(int focusChange) {
-        switch (focusChange) {
-            case AudioManager.AUDIOFOCUS_LOSS:
-                stopMusic();
-                break;
-            case AudioManager.AUDIOFOCUS_GAIN:
-                playMusic();
-                player.setVolume(1.0f, 1.0f);
-                break;
-            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-                pauseMusic();
-                break;
-            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
-                player.setVolume(0.2f, 0.2f);
-                break;
-            default:
-        }
-    }
-
-    private void pauseMusic() {
-        if (player != null) {
-            if (player.isPlaying()) {
-                songSeek = player.getCurrentPosition();
-                player.pause();
+    override fun onAudioFocusChange(focusChange: Int) {
+        when (focusChange) {
+            AudioManager.AUDIOFOCUS_LOSS -> stopMusic()
+            AudioManager.AUDIOFOCUS_GAIN -> {
+                playMusic()
+                player!!.setVolume(1.0f, 1.0f)
             }
-            PlaybackState playbackState = new PlaybackState(queueManager.getCurrentSong(),
-                    player.isPlaying(), player.getDuration(), player.getCurrentPosition());
-            eventBus.post(playbackState);
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> pauseMusic()
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> player!!.setVolume(0.2f, 0.2f)
         }
     }
 
-    private boolean requestAudioFocus() {
-        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        int result = am.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
-        return result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED;
+    private fun pauseMusic() {
+        if (player != null) {
+            if (player!!.isPlaying) {
+                songSeek = player!!.currentPosition
+                player!!.pause()
+            }
+            val playbackState = PlaybackState(queueManager.currentSong,
+                    player!!.isPlaying, player!!.duration, player!!.currentPosition)
+            eventBus.post(playbackState)
+        }
     }
 
-    private void abandonAudioFocus() {
-        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        audioManager.abandonAudioFocus(this);
+    private fun requestAudioFocus(): Boolean {
+        val am = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val result = am.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN)
+        return result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED
     }
 
-    private void registerEvents() {
-        eventBus.observable(PlayAllSongs.class)
-                .subscribe(event -> musicRepository.getAllSongs()
-                        .subscribe(songs -> {
-                            stopMusic();
-                            queueManager.setQueue(songs, event.getStartSongId());
-                            playMusic();
-                        }, error -> Log.e(TAG, error.getMessage()), () -> Log.d(TAG, "Get all songs complete")), error -> Log.e(TAG, error.getMessage()));
+    private fun abandonAudioFocus() {
+        val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        audioManager.abandonAudioFocus(this)
+    }
 
-        eventBus.observable(TogglePlayback.class)
-                .subscribe(event -> {
-                    if (player.isPlaying()) {
-                        pauseMusic();
-                        return;
+    private fun registerEvents() {
+        eventBus.observable(PlayAllSongs::class.java)
+                .subscribe({ event ->
+                    musicRepository.allSongs
+                            .subscribe({ songs ->
+                                stopMusic()
+                                queueManager.setQueue(songs, event.getStartSongId())
+                                playMusic()
+                            }, { error -> Log.e(TAG, error.message) }) { Log.d(TAG, "Get all songs complete") }
+                }) { error -> Log.e(TAG, error.message) }
+
+        eventBus.observable(TogglePlayback::class.java)
+                .subscribe { event ->
+                    if (player!!.isPlaying) {
+                        pauseMusic()
+                        return@eventBus.observable(TogglePlayback.class)
+                                .subscribe
                     }
-                    playMusic();
-                });
+                    playMusic()
+                }
 
-        eventBus.observable(RequestPlaybackState.class)
-                .subscribe(requestPlaybackState -> {
-                    Log.d(TAG, "requestPlaybackState called");
-                    int duration = player.isPlaying() ? player.getDuration() : 0;
-                    PlaybackState playbackState = new PlaybackState(queueManager.getCurrentSong(),
-                            player.isPlaying(), duration, player.getCurrentPosition());
+        eventBus.observable(RequestPlaybackState::class.java)
+                .subscribe { requestPlaybackState ->
+                    Log.d(TAG, "requestPlaybackState called")
+                    val duration = if (player!!.isPlaying) player!!.duration else 0
+                    val playbackState = PlaybackState(queueManager.currentSong,
+                            player!!.isPlaying, duration, player!!.currentPosition)
                     if (queueManager.hasSongs()) {
-                        eventBus.post(playbackState);
+                        eventBus.post(playbackState)
                     }
-                });
-        eventBus.observable(NextSong.class).subscribe(nextSong -> playNextSong());
+                }
+        eventBus.observable(NextSong::class.java).subscribe { nextSong -> playNextSong() }
 
-        eventBus.observable(PreviousSong.class).subscribe(nextSong -> playPreviousSong());
+        eventBus.observable(PreviousSong::class.java).subscribe { nextSong -> playPreviousSong() }
 
-        eventBus.observable(Seek.class).subscribe(seek -> {
-            songSeek = seek.getSeekTo();
-            player.seekTo(songSeek);
-            eventBus.post(new RequestPlaybackState());
-        }, e -> Log.e(TAG, e.getMessage()));
+        eventBus.observable(Seek::class.java).subscribe({ seek ->
+            songSeek = seek.seekTo
+            player!!.seekTo(songSeek)
+            eventBus.post(RequestPlaybackState())
+        }) { e -> Log.e(TAG, e.message) }
     }
 
-    private void playPreviousSong() {
-        pauseMusic();
-        if (player.getCurrentPosition() > 3) {
-            songSeek = 0;
+    private fun playPreviousSong() {
+        pauseMusic()
+        if (player!!.currentPosition > 3) {
+            songSeek = 0
             if (queueManager.previous() != null) {
-                playMusic();
+                playMusic()
             }
         } else {
-            songSeek = 0;
-            player.seekTo(songSeek);
+            songSeek = 0
+            player!!.seekTo(songSeek)
         }
-        eventBus.post(RequestPlaybackState.class);
+        eventBus.post(RequestPlaybackState::class.java)
     }
 
-    private void stopMusic() {
-        pauseMusic();
-        songSeek = 0;
-        abandonAudioFocus();
+    private fun stopMusic() {
+        pauseMusic()
+        songSeek = 0
+        abandonAudioFocus()
     }
 
-    private void playMusic() {
-        if (!player.isPlaying()) {
-            player.reset();
+    private fun playMusic() {
+        if (!player!!.isPlaying) {
+            player!!.reset()
             try {
                 if (queueManager.hasSongs()) {
-                    Uri uri = Uri.parse(queueManager.getCurrentSong().getSongUri());
-                    player.setDataSource(this, uri);
-                    player.prepareAsync();
+                    val uri = Uri.parse(queueManager.currentSong.songUri)
+                    player!!.setDataSource(this, uri)
+                    player!!.prepareAsync()
                 }
-            } catch (IOException e) {
-                Log.e(TAG, e.getMessage(), e);
+            } catch (e: IOException) {
+                Log.e(TAG, e.message, e)
             }
+
         }
     }
 
-    @Override
-    public void onCompletion(MediaPlayer mediaPlayer) {
-        Log.d(TAG, "Song Play complete");
-        playNextSong();
+    override fun onCompletion(mediaPlayer: MediaPlayer) {
+        Log.d(TAG, "Song Play complete")
+        playNextSong()
     }
 
-    private void playNextSong() {
-        pauseMusic();
+    private fun playNextSong() {
+        pauseMusic()
         if (queueManager.next() != null) {
-            songSeek = 0;
-            playMusic();
+            songSeek = 0
+            playMusic()
         }
-        eventBus.post(RequestPlaybackState.class);
+        eventBus.post(RequestPlaybackState::class.java)
     }
 
-    @Override
-    public boolean onError(MediaPlayer mediaPlayer, int what, int extra) {
-        return true;
+    override fun onError(mediaPlayer: MediaPlayer, what: Int, extra: Int): Boolean {
+        return true
     }
 
-    @Override
-    public void onPrepared(MediaPlayer mediaPlayer) {
+    override fun onPrepared(mediaPlayer: MediaPlayer) {
         if (requestAudioFocus()) {
-            mediaPlayer.seekTo(songSeek);
-            mediaPlayer.start();
-            PlaybackState playbackState = new PlaybackState(queueManager.getCurrentSong(),
-                    mediaPlayer.isPlaying(), mediaPlayer.getDuration(), mediaPlayer.getCurrentPosition());
-            eventBus.post(playbackState); //TODO: Check the implication of this
-            eventBus.post(new RequestPlaybackState());
+            mediaPlayer.seekTo(songSeek)
+            mediaPlayer.start()
+            val playbackState = PlaybackState(queueManager.currentSong,
+                    mediaPlayer.isPlaying, mediaPlayer.duration, mediaPlayer.currentPosition)
+            eventBus.post(playbackState) //TODO: Check the implication of this
+            eventBus.post(RequestPlaybackState())
         }
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        eventBus.cleanup();
-        player.release();
+    override fun onDestroy() {
+        super.onDestroy()
+        eventBus.cleanup()
+        player!!.release()
+    }
+
+    companion object {
+        private val TAG = "PlaybackService"
     }
 }
